4e38c77e7aa51074c96129c1b04e64da2f18d992
Engin Yenice <enginyenice2626@gmail.com>
2019-11-08 01:55:54 +0300

    Selection Sort ADD

---
+++ cProgramlama/lesson-2/selection-sort/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+void swap(int *min,int *max);
+
+
+void selection_sort(int arr[],int MAX)
+{
+    int i,k,min;
+    for(i = 0; i< MAX-1; i++)
+    {
+        min = i;
+        for(k = i+1; k< MAX; k++)
+        {
+            if(arr[k] < arr[min])
+            {
+                min = k;
+            }
+        }
+        if(min != i)
+        {
+            swap(&arr[min],&arr[i]);
+        }
+    }
+}
+
+
+void swap(int *min,int *max)
+{
+    int temp = *min;
+    *min = *max;
+    *max = temp;
+
+
+}
+void print_arr(int arr[],int MAX)
+{
+
+    for(int k = 0; k<MAX; k++)
+    {
+        printf(" %d ", arr[k]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {5,4,3,2,1,0};
+    int MAX = (sizeof(arr)/sizeof(arr[0]));
+    print_arr(arr,MAX);
+    selection_sort(arr,MAX);
+    print_arr(arr,MAX);
+
+    return 0;
+}
